0b1cf0b54cb24d4d54f59bfa8e36b3db7116969f
gideon.emmanuel <gideon.emmanuel@mukuru.com>
2023-11-14 21:32:07 +0200

    added Generics

---
+++ src/main/java/EmployeeComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class EmployeeComparator implements Comparator<StreamT.Employee> {
+
+    @Override
+    public int compare(StreamT.Employee o1, StreamT.Employee o2) {
+       return o1.getSalary().compareTo(o2.getSalary());
+    }
+
+}
---
+++ src/main/java/Generics.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Generics<T> {
+    public Generics(){}
+
+    public static <T, G> List<G> convertArr(T[] arr, Function<T, G> mapperFunc) {
+        return Arrays.stream(arr).map(mapperFunc).collect(Collectors.toList());
+    }
+
+    public void testUpperBound(Generics<? extends BasicAlgo> genric) {
+
+    }
+
+    public  <T extends TestInterface & BoundContext> void tryExtendsMultiple(T element) {
+
+    }
+    
+}
